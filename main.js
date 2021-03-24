d85361294ac6bf13bfaf11c623eee66d59853e0d
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-24 21:34:06 +0100

    -

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+const readlineSync = require('readline-sync')
 const chalk = require('chalk')
 const fs = require('fs')
+
+console.log('Exercice interraction commande cp')
 
 if (process.argv[2] === 'cp') {
 
   fs.copyFileSync('./test.txt', './copy.txt')
-  console.log(chalk.green('le fichier est copié'))
+  console.log(chalk.green('le fichier a été copié'))
  }
 else {
   console.log(chalk.red('le fichier n est pas copié'))
