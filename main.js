60e370fafb91953d7d4829d9e5503868acfca52d
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-24 19:36:45 +0100

    -

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const chalk = require('chalk')
 const fs = require('fs')
 
 if (process.argv[2] === 'cp') {
 
   fs.copyFileSync('./test.txt', './copy.txt')
-  console.log(chalk.green('le fichier est copié'))
+  console.log('le fichier est copié')
  }
 else {
-  console.log(chalk.red('le fichier n est pas copié'))
+  console.log('le fichier n est pas copié')
 }
-
